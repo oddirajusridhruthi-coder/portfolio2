06a631ca96f1c5aa77282ff8f03fc3fd5865ae4d
Dhruthi <oddiraju.sridhruthi@klh.edu.in>
2026-06-13 14:28:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Profile</h1>
+      <p>My student profile page</p>
+      <div className="flex-box">
+        <div>
+  <img
+  src="rap.jpg"
+  alt="Rapunzel"
+/>
+        </div>
+
+        <table border="1">
+          <tr>
+            <th>Name</th>
+            <td>Dhruthi</td>
+          </tr>
+          <tr>
+            <th>Age</th>
+            <td>18</td>
+          </tr>
+          <tr>
+            <th>Course</th>
+            <td>B.Tech</td>
+          </tr>
+          <tr>
+            <th>College</th>
+            <td>KLU Hyderabad</td>
+          </tr>
+        </table>
+
+      </div>
+      <div className="form-section">
+        <h2>Contact Form</h2>
+        <form>
+          <input type="text" placeholder="Enter Name" /><br /><br />
+          <input type="email" placeholder="Enter Email" /><br /><br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
